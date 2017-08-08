39780ebf3fa871f8a8f93c7605b34be86daa1bfa
Marcelo Morais <marcelo.morais@symbio.com>
2017-08-08 22:04:59 +0200

    Adding getAll books, updated the states and lifecycle
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class BooksApp extends React.Component {
+
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -10,10 +13,43 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true
+    showSearchPage: true,
+    query: '',
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      console.log('state = ', this.state.books);
+    })
+
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
   }
 
   render() {
+    const { query, books } = this.state
+
+    let showingBooks
+    console.log('render -> query = ', query, 'books = ', books);
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      console.log('render -> books = ', books)
+      showingBooks = books.filter((books) => match.test(books.title))
+    } else {
+      showingBooks = books
+    }
+
+    showingBooks.sort(sortBy('title'))
+    console.log('showingBooks = ', showingBooks)
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -21,20 +57,27 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/* 
+                {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
+
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
-                
+                <input type="text"
+                placeholder="Search by title or author"
+                onChange={(event) => {
+                  this.updateQuery(event.target.value)
+                  console.log("to vivo!")
+                  }
+                } />
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              <h1>Oi</h1>
+              </ol>
             </div>
           </div>
         ) : (
